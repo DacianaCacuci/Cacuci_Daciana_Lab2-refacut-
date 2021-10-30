ee67b81a1fe4d06f592fa82dc0cd146d647613ca
Daciana Cacuci <c.daciana2000@gmail.com>
2021-10-30 14:54:41 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Cacuci_Daciana_Lab2_refacut_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
